550b06b8a5ff7491e2341c3067b84d1c7f3337c6
Muhammad Ekramah <muhammadekramah786@gmail.com>
2025-08-29 07:52:23 +0500

    create rosalind_rna.py

---
+++ rosalind_solutions/rosalind_rna.py
@@ -0,0 +1,19 @@
+# ^_^ coding:utf-8 ^_^
+
+"""
+Transcribing DNA into RNA 
+url: http://rosalind.info/problems/rna/
+
+Given: A DNA string  having length at most 1000 nt.
+Return: The transcribed RNA string of .
+"""
+
+def dna2rna(string):
+    return string.replace('T','U')
+
+if __name__ == "__main__":
+    with open("/media/ekramah/DISK2/Rosaland_Problams_Solution/rosalind_daily_challenge/rosalind_problems/rosalind_rna.txt", 'r') as f:
+        string = f.readline().strip()
+        rst = dna2rna(string)
+        print(rst)
+
